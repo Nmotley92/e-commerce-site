811bccb03dae066f79591bbfa701c22d56338931
Nathaniel Motley <nmotley92@gmail.com>
2023-03-22 01:45:08 -0500

    styled the footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,5 +1,16 @@
+import { Container, Row, Col } from 'react-bootstrap';
+
 const Footer = () => {
-    return <p> this is a footer </p>;
+    return (
+        <footer>
+        <Container fluid>
+            <Row className="mt-5">
+                <Col className="bg-dark text-white text-center py-5">CopyRight &copy; Shop Name</Col>
+            </Row>
+        </Container>
+        </footer>
+
+    )
 }
 
 export default Footer;
