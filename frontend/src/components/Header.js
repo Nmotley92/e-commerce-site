7dae50a406f26f294cb9f72b9219ab17ea5502ce
Nathaniel Motley <nmotley92@gmail.com>
2023-03-22 01:28:04 -0500

    adds styling to header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,6 @@
 import { Navbar, Nav, NavDropdown, Container, Badge, Form, Dropdown, DropdownButton, Button, InputGroup } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+import { Link } from 'react-router-dom';
 
 
 const Header = () => {
@@ -7,41 +8,55 @@ const Header = () => {
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
                 <LinkContainer to="/">
-                <Navbar.Brand href="/">Shop Name</Navbar.Brand>
+                    <Navbar.Brand href="/">Shop Name</Navbar.Brand>
                 </LinkContainer>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
                         <InputGroup>
-                        <DropdownButton id="dropdown-basic-button" title="All">
-                            <Dropdown.Item>Electronics</Dropdown.Item>
-                            <Dropdown.Item>Books</Dropdown.Item>
-                            <Dropdown.Item>Cars</Dropdown.Item>
+                            <DropdownButton id="dropdown-basic-button" title="All">
+                                <Dropdown.Item>Electronics</Dropdown.Item>
+                                <Dropdown.Item>Books</Dropdown.Item>
+                                <Dropdown.Item>Cars</Dropdown.Item>
 
-                        </DropdownButton>
-                        <Form.Control type="text" placeholder="Search in shop.." />
-                        <Button variant="warning"><i className="bi bi-search text-dark"></i></Button>
+                            </DropdownButton>
+                            <Form.Control type="text" placeholder="Search in shop.." />
+                            <Button variant="warning"><i className="bi bi-search text-dark"></i></Button>
                         </InputGroup>
-                        </Nav>
+                    </Nav>
                     <Nav>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
-                        <Nav.Link href="#pricing">
+                        <LinkContainer to="/admin/orders">
+                            <Nav.Link>
+                                Admin
+                                <span className="position-absolute top-1 start-10 translate-middle p-2 bg-danger border border-light rounded-circle"></span>
+
+                            </Nav.Link>
+                        </LinkContainer>
+
+                        <NavDropdown title="John Doe" id="collasible-nav-dropdown">
+                            <NavDropdown.Item eventKey="/user/my-orders" as={Link} to="/user/my-orders">My orders</NavDropdown.Item>
+                            <NavDropdown.Item eventKey="/user" as={Link} to="/user">My Profile</NavDropdown.Item>
+                            <NavDropdown.Item>Logout</NavDropdown.Item>
+                        </NavDropdown>
+                        <LinkContainer to="/login">
+                            <Nav.Link>
+                                Login
+                            </Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/register">
+                            <Nav.Link>
+                                Register
+                            </Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/cart">
+                            <Nav.Link>
                             <Badge bg="danger">
                                 2
                             </Badge>
-                            Cart</Nav.Link>
-                        <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
-                                Separated link
-                            </NavDropdown.Item>
-                        </NavDropdown>
+                            <i className="bi bi-cart-dash"></i>
+                            <span className="ms-1">Cart</span>
+                            </Nav.Link>
+                        </LinkContainer>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
