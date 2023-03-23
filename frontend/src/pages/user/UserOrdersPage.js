a67f5070c1d5ccee5d1fe68c49d8aea10af59c73
Nathaniel Motley <nmotley92@gmail.com>
2023-03-22 22:20:56 -0500

    adds user orders page

--- frontend/src/pages/user/UserOrdersPage.js
+++ frontend/src/pages/user/UserOrdersPage.js
@@ -1,5 +1,43 @@
+import { Row, Col, Table } from "react-bootstrap";
+import { Link } from "react-router-dom";
 const UserOrdersPage = () => {
-    return <p>This is a user orders page</p>;
-}
+  return (
+    <Row className="m-5">
+      <Col md={12}>
+        <h1>My Orders</h1>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>User</th>
+              <th>Date</th>
+              <th>Total</th>
+              <th>Delivered</th>
+              <th>Order details</th>
+            </tr>
+          </thead>
+          <tbody>
+            {["bi bi-check-lg text-success", "bi bi-x-lg text-danger"].map(
+              (item, idx) => (
+                <tr key={idx}>
+                  <td>{idx +1}</td>
+                  <td>Mark Twain</td>
+                  <td>2022-09-12</td>
+                  <td>$124</td>
+                  <td>
+                    <i className={item}></i>
+                  </td>
+                  <td>
+                    <Link to="/user/order-details">go to order</Link>
+                  </td>
+                </tr>
+              )
+            )}
+          </tbody>
+        </Table>
+      </Col>
+    </Row>
+  );
+};
 
 export default UserOrdersPage;
