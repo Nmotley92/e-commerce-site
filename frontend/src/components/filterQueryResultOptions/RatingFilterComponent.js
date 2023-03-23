1e3439217d9e15970624699b3800ffe5de5cb094
Nathaniel Motley <nmotley92@gmail.com>
2023-03-22 19:31:50 -0500

    changes star rating package to one that works

--- frontend/src/components/filterQueryResultOptions/RatingFilterComponent.js
+++ frontend/src/components/filterQueryResultOptions/RatingFilterComponent.js
@@ -1,6 +1,6 @@
-import { Rating } from "react-simple-star-rating";
-import { Form } from "react-bootstrap";
-import { Fragment } from "react";
+import React, { Fragment } from 'react';
+import Rating from 'react-rating';
+import { Form } from 'react-bootstrap';
 
 const RatingFilterComponent = () => {
   return (
@@ -10,8 +10,15 @@ const RatingFilterComponent = () => {
         <Fragment key={idx}>
           <Form.Check type="checkbox" id={`check-api-${idx}`}>
             <Form.Check.Input type="checkbox" isValid />
-            <Form.Check.Label style={{ cursor: "pointer" }}>
-              <Rating readonly size={20} initialValue={5 - idx} />
+            <Form.Check.Label style={{ cursor: 'pointer' }}>
+              <Rating
+              className="custom-rating"
+                readonly
+                emptySymbol="far fa-star"
+                fullSymbol="fas fa-star"
+                fractions={1}
+                initialRating={5 - idx}
+              />
             </Form.Check.Label>
           </Form.Check>
         </Fragment>
