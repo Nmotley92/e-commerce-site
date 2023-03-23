fa30ba1a892c3fcdd89714d4398cbbe1f72b1fd0
Dfletch <plebeianpony@gmail.com>
2023-03-23 16:59:57 -0500

    fixed seeder.js for new seeds

--- backend/seeder/seeder.js
+++ backend/seeder/seeder.js
@@ -1,19 +1,33 @@
-const connectDB = require('../config/db');
-connectDB();
+const connectDB = require("../config/db")
+connectDB()
 
-const categoryData = require('./categories');
-const Category = require('../models/CategoryModel');
+const categoryData = require("./categories")
+const productData = require("./products")
+const reviewData = require("./reviews")
+
+const Category = require("../models/CategoryModel")
+const Product = require("../models/ProductModel")
+const Review = require("../models/ReviewModel")
 
 const importData = async () => {
     try {
-        await Category.collection.deleteMany({});
-        await Category.insertMany(categoryData);
-        console.log('Data imported');
-        process.exit();
+        await Category.collection.dropIndexes()
+        await Product.collection.dropIndexes()
+
+        await Category.collection.deleteMany({})
+        await Product.collection.deleteMany({})
+        await Review.collection.deleteMany({})
+
+        await Category.insertMany(categoryData)
+        await Product.insertMany(productData)
+        await Review.insertMany(reviewData)
+
+        console.log("Data imported successfully")
+        process.exit()
     } catch (error) {
-        console.error("Error with data import", error);
+        console.error("Failed to import data", error)
         process.exit(1);
     }
-};
-
-importData();
+}
+importData()
+ 
