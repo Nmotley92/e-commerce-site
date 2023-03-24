79da788b4413f65e1c5298feff698fa927f1c22a
Dfletch <plebeianpony@gmail.com>
2023-03-23 19:52:03 -0500

    added pagination config file and products controller limits per page

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,8 +1,17 @@
-const Product = require('../models/productModel')
+const Product = require("../models/ProductModel")
+const recordsPerPage = require("../config/pagination.js")
 
-const getProducts = (req, res) => {
-
-    res.send('Handling Products routes')
-}
-
+const getProducts = async (req, res, next) => {
+    try {
+      const pageNum = Number(req.query.pageNum) || 1;
+      // res.json({pageNum})
+      const products = await Product.find({})
+          .skip(recordsPerPage * (pageNum - 1))
+        .sort({ name: 1 })
+        .limit(recordsPerPage);
+      res.json({ products });
+    } catch (error) {
+      next(error);
+    }
+  };
 module.exports = getProducts
