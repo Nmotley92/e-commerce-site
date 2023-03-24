94d01b7acc46dc335e1acce9583ce33f9ea3cc70
Dfletch <plebeianpony@gmail.com>
2023-03-23 20:27:52 -0500

    get product by attributes

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -4,39 +4,64 @@ const recordsPerPage = require("../config/pagination");
 const getProducts = async (req, res, next) => {
   try {
     let query = {};
-    let queryCondition = false
+    let queryCondition = false;
 
     let priceQueryCondition = {};
     if (req.query.price) {
-        queryCondition = true;
+      queryCondition = true;
       priceQueryCondition = { price: { $lte: Number(req.query.price) } };
     }
     let ratingQueryCondition = {};
     if (req.query.rating) {
-        queryCondition = true;
+      queryCondition = true;
       ratingQueryCondition = { rating: { $in: req.query.rating.split(",") } };
     }
-    let categoryQueryCondition = {}
-    const categoryName = req.params.categoryName || ""
-    if(categoryName) {
-        queryCondition = true
-        let a = categoryName.replaceAll(",", "/")
-        var regEx = new RegExp("^" + a)
-        categoryQueryCondition = { category: regEx }
+    let categoryQueryCondition = {};
+    const categoryName = req.params.categoryName || "";
+    if (categoryName) {
+      queryCondition = true;
+      let a = categoryName.replaceAll(",", "/");
+      var regEx = new RegExp("^" + a);
+      categoryQueryCondition = { category: regEx };
     }
-    if(req.query.category) {
-        queryCondition = true
-        let a = req.query.category.split(",").map((item) => {
-            if(item) return new RegExp("^" + item)
-        })
-        categoryQueryCondition = {
-            category: { $in: a }
-        }
+    if (req.query.category) {
+      queryCondition = true;
+      let a = req.query.category.split(",").map((item) => {
+        if (item) return new RegExp("^" + item);
+      });
+      categoryQueryCondition = {
+        category: { $in: a },
+      };
+    }
+    let attrsQueryCondition = [];
+    if (req.query.attrs) {
+      // attrs=RAM-1TB-2TB-4TB,color-blue-red
+      // [ 'RAM-1TB-4TB', 'color-blue', '' ]
+      attrsQueryCondition = req.query.attrs.split(",").reduce((acc, item) => {
+        if (item) {
+          let a = item.split("-");
+          let values = [...a];
+          values.shift(); // removes first item
+          let a1 = {
+            attrs: { $elemMatch: { key: a[0], value: { $in: values } } },
+          };
+          acc.push(a1);
+          // console.dir(acc, { depth: null })
+          return acc;
+        } else return acc;
+      }, []);
+    //   console.dir(attrsQueryCondition, { depth: null });
+    queryCondition = true;
     }
 
     if (queryCondition) {
       query = {
-        $and: [priceQueryCondition, ratingQueryCondition, categoryQueryCondition],
+        $and: [
+          priceQueryCondition,
+          ratingQueryCondition,
+          categoryQueryCondition,
+          ...attrsQueryCondition
+        ],
       };
     }
 
