8626d7bd70e53cec848daa8fbac23215b8bf7abe
Dfletch <plebeianpony@gmail.com>
2023-03-23 19:53:51 -0500

    added link number formula to product controller

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,17 +1,23 @@
-const Product = require("../models/ProductModel")
-const recordsPerPage = require("../config/pagination.js")
+const Product = require("../models/ProductModel");
+const recordsPerPage = require("../config/pagination");
 
 const getProducts = async (req, res, next) => {
-    try {
-      const pageNum = Number(req.query.pageNum) || 1;
-      // res.json({pageNum})
-      const products = await Product.find({})
-          .skip(recordsPerPage * (pageNum - 1))
-        .sort({ name: 1 })
-        .limit(recordsPerPage);
-      res.json({ products });
-    } catch (error) {
-      next(error);
-    }
-  };
-module.exports = getProducts
+  try {
+    const pageNum = Number(req.query.pageNum) || 1;
+    const totalProducts = await Product.countDocuments({});
+    const products = await Product.find({})
+      .skip(recordsPerPage * (pageNum - 1))
+      .sort({ name: 1 })
+      .limit(recordsPerPage);
+      
+    res.json({
+      products,
+      pageNum,
+      paginationLinksNumber: Math.ceil(totalProducts / recordsPerPage),
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+module.exports = getProducts;
+
