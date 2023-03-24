7c1336c8d9ac93cacb9c3d3bf374348ecf0ef6a9
Dfletch <plebeianpony@gmail.com>
2023-03-23 20:02:39 -0500

    added filtering for products.

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -3,22 +3,43 @@ const recordsPerPage = require("../config/pagination");
 
 const getProducts = async (req, res, next) => {
   try {
-    const pageNum = Number(req.query.pageNum) || 1;
-    const totalProducts = await Product.countDocuments({});
+    let query = {};
+    let queryCondition = false
 
-    // sort options
-    let sort = {}
-    const sortOption = req.query.sort || ""
-    if(sortOption) {
-        let sortOpt = sortOption.split("_")
-        sort = { [sortOpt[0]]: Number(sortOpt[1]) }
+    let priceQueryCondition = {};
+    if (req.query.price) {
+        queryCondition = true;
+      priceQueryCondition = { price: { $lte: Number(req.query.price) } };
+    }
+    let ratingQueryCondition = {};
+    if (req.query.rating) {
+        queryCondition = true;
+      ratingQueryCondition = { rating: { $in: req.query.rating.split(",") } };
     }
 
-    const products = await Product.find({})
+    if (queryCondition) {
+      query = {
+        $and: [priceQueryCondition, ratingQueryCondition],
+      };
+    }
+
+    //pagination
+    const pageNum = Number(req.query.pageNum) || 1;
+
+    // sort by name, price etc.
+    let sort = {};
+    const sortOption = req.query.sort || "";
+    if (sortOption) {
+      let sortOpt = sortOption.split("_");
+      sort = { [sortOpt[0]]: Number(sortOpt[1]) };
+    }
+
+    const totalProducts = await Product.countDocuments(query);
+    const products = await Product.find(query)
       .skip(recordsPerPage * (pageNum - 1))
-      .sort({ name: 1 })
+      .sort(sort)
       .limit(recordsPerPage);
-      
+
     res.json({
       products,
       pageNum,
@@ -29,4 +50,3 @@ const getProducts = async (req, res, next) => {
   }
 };
 module.exports = getProducts;
-
