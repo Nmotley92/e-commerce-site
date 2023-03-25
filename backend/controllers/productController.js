80355ff07d1efacaed4146b76f31df310b4a4c66
Dfletch <plebeianpony@gmail.com>
2023-03-25 11:49:42 -0500

    finished best seller controller

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -117,7 +117,12 @@ const getProductById = async (req, res, next) => {
 const getBestsellers = async (req, res, next) => {
   try {
       const products = await Product.aggregate([
-          { $limit: 3 }
+        { $sort: { category: 1, sales: -1 } },
+        { $group: { _id: "$category", doc_with_max_sales: { $first: "$$ROOT" } } },
+        { $replaceWith: "$doc_with_max_sales" },
+        { $match: { sales: { $gt: 0 } } },
+        { $project: { _id: 1, name: 1, images: 1, category: 1, description: 1 } },
+        { $limit: 3 }
       ])
       res.json(products)
   } catch(error) {
