9711c44e7a5262fec0e692f3f8731ef130b93315
Dfletch <plebeianpony@gmail.com>
2023-03-27 18:48:51 -0500

    You wrote and support wrote messages.

--- frontend/src/components/user/UserChat.js
+++ frontend/src/components/user/UserChat.js
@@ -1,21 +1,48 @@
 import "../../chats.css";
 import { useEffect, useState } from "react";
 import socketIOClient from "socket.io-client";
+import { useSelector } from "react-redux";
 
 const UserChat = () => {
   const [socket, setSocket] = useState(false);
+  //   let chat = [
+  //       {"client": "msg"},
+  //       {"client": "msg"},
+  //       {"admin": "msg"},
+  //   ]
+  const [chat, setChat] = useState([]);
+
+  const userInfo = useSelector((state) => state.userRegisterLogin.userInfo);
+
   useEffect(() => {
+    if (!userInfo.isAdmin) {
       const socket = socketIOClient();
       setSocket(socket);
       return () => socket.disconnect();
-  }, [])
+    }
+  }, [userInfo.isAdmin]);
 
   const clientSubmitChatMsg = (e) => {
-      if (e.keyCode && e.keyCode !== 13) {
-          return
-      }
-      socket.emit("client sends message", "message from client");
-  }
+    if (e.keyCode && e.keyCode !== 13) {
+      return;
+    }
+    const msg = document.getElementById("clientChatMsg");
+    let v = msg.value.trim();
+    if (v === "" || v === null || v === false || !v) {
+      return;
+    }
+    socket.emit("client sends message", v);
+    setChat((chat) => {
+      return [...chat, { client: v }];
+    });
+    msg.focus();
+    setTimeout(() => {
+         msg.value = "";
+         const chatMessages = document.querySelector(".cht-msg");
+         chatMessages.scrollTop = chatMessages.scrollHeight;
+    }, 200)
+  };
+
     return (
         <>
         <input type="checkbox" id="check" />
@@ -30,24 +57,29 @@ const UserChat = () => {
           </div>
           <div className="chat-form">
             <div className="cht-msg">
-              {Array.from({ length: 20 }).map((_, id) => (
-                <div key={id}>
+            {chat.map((item, id) => (
+              <div key={id}>
+                {item.client && (
                   <p>
-                    <b>You wrote:</b> Hello, world! This is a toast message.
+                    <b>You wrote:</b>
                   </p>
+                  )}
+                  {item.admin && (
                   <p className="bg-primary p-3 ms-4 text-light rounded-pill">
-                    <b>Support wrote:</b> Hello, world! This is a toast message.
+                    <b>Support wrote:</b> 
                   </p>
+                  )}
                 </div>
               ))}
             </div>
             <textarea
+              onKeyUp={(e) => clientSubmitChatMsg(e)}
               id="clientChatMsg"
               className="form-control"
               placeholder="Your Text Message"
             ></textarea>
   
-            <button className="btn btn-success btn-block">Submit</button>
+            <button onClick={(e) => clientSubmitChatMsg(e)} className="btn btn-success btn-block">Submit</button>
           </div>
         </div>
       </>
