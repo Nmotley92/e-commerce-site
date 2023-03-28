0f326c1f19b7f0d18650efaaa01174586658fa2a
Nathaniel Motley <nmotley92@gmail.com>
2023-03-27 19:24:54 -0500

    register user is fucntional

--- frontend/src/components/admin/AdminLinksComponent.js
+++ frontend/src/components/admin/AdminLinksComponent.js
@@ -1,7 +1,11 @@
 import { Nav, Navbar } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 
+import { logout } from "../../redux/actions/userActions";
+import { useDispatch } from "react-redux";
+
 const AdminLinksComponent = () => {
+    const dispatch = useDispatch()
   return (
     <Navbar bg="light" variant="light">
       <Nav className="flex-column">
@@ -20,10 +24,11 @@ const AdminLinksComponent = () => {
         <LinkContainer to="/admin/analytics">
           <Nav.Link>Analytics</Nav.Link>
         </LinkContainer>
-        <Nav.Link>Logout</Nav.Link>
+        <Nav.Link onClick={() => dispatch(logout())}>Logout</Nav.Link>
       </Nav>
     </Navbar>
   );
 };
 
 export default AdminLinksComponent;
+
